f270bffe52ce9729a7349b82ba17943838aa6fd0
proyectosss <43769989+proyectosss@users.noreply.github.com>
2018-10-04 23:23:26 -0500

    modelos
    modelos

---
+++ follow/bd/conexion.php
@@ -0,0 +1,23 @@
+<?php
+class conexion
+{
+    public static function getConnection()
+    {
+        $conn = new mysqli('localhost', 'root', '', 'db_follow');
+        $conn->query("SET NAMES 'utf8'");
+        if ($conn->connect_error) {
+            die('no se pudo conectar a la base de dato' . $conn->connect_error);
+        } else {
+            return $conn;
+        }
+    }
+
+    public function protect($v)
+    {
+        $conn = new mysqli('localhost', 'root', '', 'db_follow');
+        $v    = mysqli_real_escape_string($conn, $v);
+        $v    = htmlentities($v, ENT_QUOTES);
+        $V    = trim($v);
+        return $v;
+    }
+}
